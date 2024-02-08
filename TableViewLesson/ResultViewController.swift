7d68bba2870ce9845a18b1ed3eabbb55da738d6a
bekturinho <ismanalievbektur4@gmail.com>
2024-02-08 05:31:16 -0800

    вы

---
+++ TableViewLesson/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  TableViewLesson
+//
+//  Created by fortune cookie on 2/8/24.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TableViewLesson/progLang.swift
@@ -0,0 +1,8 @@
+//
+//  progLang.swift
+//  TableViewLesson
+//
+//  Created by fortune cookie on 2/8/24.
+//
+
+import Foundation
